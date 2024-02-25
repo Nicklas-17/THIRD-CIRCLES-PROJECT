47c5176b1ef5c493976baaa9a639f5da0efd63db
Nicklas17 <116688710+Nicklas-17@users.noreply.github.com>
2024-02-25 17:21:16 +0000

    Improvement on Layout Responsiveness

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ const month = document.querySelectorAll(".month");
 month.forEach((el) => observer.observe(el));
 
 gsap.registerPlugin(ScrollTrigger);
+
+/* trailer */
+
+const trailer = document.getElementById("trailer");
+
+const getTrailerClass = (type) => {
+  switch (type) {
+    case "video":
+      return "fi fi-sr-play";
+
+    default:
+      return "fa-solid fa-arrow-up-right";
+      break;
+  }
+};
+
+const animateTrailer = (e, interacting) => {
+  const x = e.clientX - trailer.offsetWidth / 2,
+    y = e.clientY - trailer.offsetHeight / 2;
+
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 7 : 1})`,
+  };
+
+  trailer.animate(keyframes, { duration: 800, fill: "forwards" });
+};
+
+window.onmousemove = (e) => {
+  const interactable = e.target.closest(".interactable"),
+    interacting = interactable !== null;
+
+  animateTrailer(e, interacting);
+
+  trailer.dataset.type = interacting ? interactable.dataset.type : "";
+
+  if (interacting) {
+    icon.className = getTrailerClass(interactable.dataset.type);
+  }
+};
